9f2c45066ef9563a1b33fac5376d112e682f6a7f
Paulo-USP-11234360 <paulooliveira12134@usp.br>
2023-11-25 15:41:18 -0300

    verificao interna

--- testes/teste_senha.py
+++ testes/teste_senha.py
@@ -1,5 +1,12 @@
 from ..scripts.senha import check_password
 
+# Define uma função de teste chamada test_verificar_senha
 def test_verificar_senha():
-    assert check_password("senha_correta") is True
-    assert check_password("senha_incorreta") is False
+    # Cria uma variável chamada stored_hashed_password e atribui um valor fictício a ela
+    stored_hashed_password = "senha123"  # Substitua com o valor real
+    
+    # Testa se a função check_password retorna True para a senha "senha_correta"
+    assert check_password("senha_correta", stored_hashed_password) is True, "Validação para senha correta"
+    
+    # Testa se a função check_password retorna False para a senha "senha_incorreta"
+    assert check_password("senha_incorreta", stored_hashed_password) is False, "Validação para senha incorreta"
